ef923c5f136d3b121eb038e7b999140a41e15d58
hwclass <harderworking.class@gmail.com>
2014-10-13 02:34:30 +0300

    Added the main library file as objeckt-0.1.0.js

---
+++ objeckt-0.1.0.js
@@ -0,0 +1,16 @@
+/*!
+ * Objeckt : An object creation and management library also using inheritance as a OOP paradigm.
+ *
+ * Copyright (c) 2014 Barış Güler
+ * http://hwclass.github.io
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Launch  : October 2014
+ * Version : 0.1.0
+ * Released: 2014
+ *
+ */
+
+ 
